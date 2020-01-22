b8da7c601d21bf53fd04c037815bd50051410ee3
Aline Gasparindo <aline.gasparindo@gmail.com>
2020-01-21 20:17:30 -0500

    done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+
+  end
+
+  def answer
+    @question = params[:question]
+    if @question == "I am going to work right now!"
+      @answer = "Great"
+    elsif @question.include? "?"
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  get 'ask', to: 'questions#ask', as: :ask
+  get 'answer', to: 'questions#answer', as: :answer
+
+  # post 'answer', to: 'questions#answer'
+
+  root to: 'questions#ask'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
